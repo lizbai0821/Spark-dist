a0254599aa068c7b06a5ba431775a5dcdb3b6cb1
Chris Liu <cscyliu@comp.polyu.edu.hk>
2016-11-29 20:22:36 +0800

    prepare for 20161129

---
+++ spark-dist/conf/spark-env.sh
@@ -0,0 +1,6 @@
+base_path=/opt/spark_test
+
+export SPARK_LOCAL_HOSTNAME=$(hostname)
+export JAVA_HOME=/usr/java/jdk1.8.0_20
+export HADOOP_HOME=/opt/wangxp/hadoop-2.7.0
+export HADOOP_CONF_DIR=${HADOOP_HOME}/etc/hadoop
